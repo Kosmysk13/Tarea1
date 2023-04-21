cae3ae1d8088b2e99d33b4add44ba4ac54c9f27b
c4milot <camilotapiacorrea@gmail.com>
2023-04-21 00:56:11 -0400

    refactor: creacion de archivos por cada clase

---
+++ CocaCola.java
@@ -0,0 +1,8 @@
+class CocaCola extends Bebida{
+    public CocaCola(int s){
+        super(s);
+    }
+    public String beber(){
+        return "cocacola";
+    }
+}
---
+++ Moneda.java
@@ -0,0 +1,11 @@
+abstract class Moneda {
+    public Moneda() {
+
+    }
+
+    public Moneda getSerie() {
+        return this;
+    }
+
+    public abstract int getValor();
+}
