a507ea9d068fe8890f98feda6408ee86874e40bb
Kosmysk13 <durbano2020@udec.cl>
2023-04-28 14:07:57 -0400

    Update Excepciones

--- NoHayProductoException.java
+++ NoHayProductoException.java
@@ -1,7 +1,6 @@
 package org.example;
 
 public class NoHayProductoException extends Exception{
-    public static final long serialVersionUID = 401;
     public NoHayProductoException(String errorMensaje){
         super(errorMensaje);
     }
--- PagoIncorrectoException.java
+++ PagoIncorrectoException.java
@@ -1,7 +1,6 @@
 package org.example;
 
 public class PagoIncorrectoException extends Exception{
-    public static final long serialVersionUID = 400;
     public PagoIncorrectoException(String errorMessage){
         super(errorMessage);
     }
